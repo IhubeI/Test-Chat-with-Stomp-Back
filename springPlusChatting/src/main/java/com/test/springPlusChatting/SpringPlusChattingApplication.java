a390b22193d248fa7205ad6c4872cf1f6b269c6b
lhubel <jkl5914@naver.com>
2024-08-02 17:31:27 +0900

    채팅 프로그램 구현해볼 요소

---
+++ springPlusChatting/src/main/java/com/test/springPlusChatting/SpringPlusChattingApplication.java
@@ -0,0 +1,13 @@
+package com.test.springPlusChatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlusChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlusChattingApplication.class, args);
+	}
+
+}
