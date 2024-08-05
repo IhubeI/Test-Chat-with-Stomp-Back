f4353bec3e04190133020ffe87c52a95b57feacc
lhubel <jkl5914@naver.com>
2024-08-05 15:20:35 +0900

    핸들러 수정중

--- springPlusChatting/src/main/java/com/test/springPlusChatting/handler/ChattingWebsocketHandler.java
+++ springPlusChatting/src/main/java/com/test/springPlusChatting/handler/ChattingWebsocketHandler.java
@@ -1,14 +1,19 @@
 package com.test.springPlusChatting.handler;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.test.springPlusChatting.service.ChattingService;
 
 public class ChattingWebsocketHandler extends TextWebSocketHandler {
-	
+
 	// SLF4J를 이용하여 로거를 남기기 위한 과정
 	// https://enai.tistory.com/36
 	// 나중에 더 좋은 라이브러리가 생겨 교체하더라도, application의 코드를 변경할 필요가 없다는 장점이 있다.
@@ -16,5 +21,8 @@ public class ChattingWebsocketHandler extends TextWebSocketHandler {
 
 	@Autowired
 	private ChattingService service;
+
+	private Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<WebSocketSession>());
+
 	
 }
